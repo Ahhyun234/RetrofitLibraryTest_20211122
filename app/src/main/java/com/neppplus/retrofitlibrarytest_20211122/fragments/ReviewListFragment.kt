fe9878becd99b8f99229f80649d1225c1536ddc7
이아현 <suk03029@gmail.com>
2021-11-24 15:22:56 +0900

    어댑터와 리사이클러뷰 연결 작업 함

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fragments/ReviewListFragment.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fragments/ReviewListFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.neppplus.retrofitlibrarytest_20211122.R
+import com.neppplus.retrofitlibrarytest_20211122.adapters.CategoryRecyclerAdapter
 import com.neppplus.retrofitlibrarytest_20211122.databinding.FragmentReviewListBinding
 import com.neppplus.retrofitlibrarytest_20211122.datas.BasicResponse
 import com.neppplus.retrofitlibrarytest_20211122.datas.SmallCategoryData
@@ -18,6 +21,7 @@ class ReviewListFragment : BaseFragment() {
 
     lateinit var binding: FragmentReviewListBinding
     val mCategoryList = ArrayList<SmallCategoryData>()
+    lateinit var mCategoryAddapter : CategoryRecyclerAdapter
 
 
     override fun onCreateView(
@@ -43,6 +47,10 @@ class ReviewListFragment : BaseFragment() {
     override fun setValues() {
         getCategoryListFromServer()
 
+        mCategoryAddapter = CategoryRecyclerAdapter(mContext,mCategoryList)
+        binding.categoryRecyclerView.adapter = mCategoryAddapter
+        binding.categoryRecyclerView.layoutManager = LinearLayoutManager(mContext)
+
     }
 
     fun getCategoryListFromServer(){
@@ -53,6 +61,8 @@ class ReviewListFragment : BaseFragment() {
 
                     mCategoryList.clear()
                     mCategoryList.addAll(br.data.categories)
+                    mCategoryAddapter.notifyDataSetChanged()
+
                 }
 
 
