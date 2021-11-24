15be111f7367c967cd9f63c27f83df3527dbcd7a
이아현 <suk03029@gmail.com>
2021-11-24 16:34:44 +0900

    StaggeredGridLayoutManager를 사용해봄

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fragments/ProductListFragment.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/fragments/ProductListFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.neppplus.retrofitlibrarytest_20211122.R
 import com.neppplus.retrofitlibrarytest_20211122.adapters.ProductAdapter
 import com.neppplus.retrofitlibrarytest_20211122.adapters.ProductRecyclerViewAdapter
@@ -55,7 +56,7 @@ class ProductListFragment : BaseFragment() {
 
         mProductRecyclerAdapter = ProductRecyclerViewAdapter(mContext,mProductList)
         binding.productListRecyclerView.adapter = mProductRecyclerAdapter
-        binding.productListRecyclerView.layoutManager = LinearLayoutManager(mContext)
+        binding.productListRecyclerView.layoutManager = StaggeredGridLayoutManager(2,LinearLayoutManager.VERTICAL)
 //
 //        mProductAdapter = ProductAdapter(mContext, R.layout.product_list_item, mProductList)
 //        binding.productListView.adapter = mProductAdapter
