ecf77f59317ba6341e6cca191c358fcd12c4c7e2
이아현 <suk03029@gmail.com>
2021-11-24 16:55:27 +0900

    자동 로그인 검사기준 변경 - 저장된 코튼으로 내 정보 불러내보기

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/SplashActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/SplashActivity.kt
@@ -1,9 +1,18 @@
 package com.neppplus.retrofitlibrarytest_20211122
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarytest_20211122.databinding.ActivitySplashBinding
+import com.neppplus.retrofitlibrarytest_20211122.datas.BasicResponse
+import com.neppplus.retrofitlibrarytest_20211122.datas.UserData
+import com.neppplus.retrofitlibrarytest_20211122.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : BaseActivity() {
 
@@ -20,6 +29,37 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        var loginUser : UserData? = null
+        apiService.getRequestMyInfo(ContextUtil.getToken(mContext)).enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful  ){
+                    loginUser = response.body()!!.data.user
+
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+            val myIntent : Intent
+
+            if (loginUser != null){
+                myIntent = Intent(mContext,MainActivity::class.java)
+
+            }
+            else{
+                myIntent = Intent.parseIntent(mContext,LoginActivity::class.java)
+
+                            }
+            startActivity(myIntent)
+        })
 
     }
 
