e371267e6623a97d6f7080fccbed306f930b0eaf
이아현 <suk03029@gmail.com>
2021-11-24 16:02:36 +0900

    추가 파싱

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/datas/ProductData.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/datas/ProductData.kt
@@ -6,10 +6,16 @@ class ProductData(
 
     var id: Int,
     var name: String,
+    var price : Int,
+    @SerializedName("image_url")
+    var imageUrl : String,
+
 
     var store: StoreData,
     @SerializedName("small_category")
     var smallCategory : SmallCategoryData
+
+
 
 
 ) {
