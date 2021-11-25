73e366860eb25b12ebf628d1426e8d32eee6748c
이아현 <suk03029@gmail.com>
2021-11-25 12:03:20 +0900

    리뷰 작성 화면의 기본 정보 표시 완려ㅛ

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/EditReviewActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/EditReviewActivity.kt
@@ -7,6 +7,8 @@ import com.neppplus.retrofitlibrarytest_20211122.databinding.ActivityEditReviewB
 import com.neppplus.retrofitlibrarytest_20211122.databinding.ActivitySplashBinding
 import com.neppplus.retrofitlibrarytest_20211122.datas.ProductData
 import com.neppplus.retrofitlibrarytest_20211122.utils.GlobalData
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditReviewActivity : BaseActivity() {
 
@@ -27,6 +29,15 @@ class EditReviewActivity : BaseActivity() {
         mProductData = intent.getSerializableExtra("product") as ProductData
         binding.txtProductName.text = mProductData.name
         binding.txtUserNickname.text = GlobalData.logInUser!!.nickname
+
+//        오늘 날짜를 가져와서 이걸 2021.11.25 형태로 가공 -> 텍스트뷰에 반영
+//        1. 오늘 날짜?
+        val now = Calendar.getInstance() //현재 일시 자동 기록
+//        원하는 형채로 가공
+        val sdf = SimpleDateFormat("yyyy.M.D")
+        val nowString = sdf.format(now.time)
+
+        binding.txtToday.text = nowString
 
     }
 }
