dfd0ad1659d46b9cdf2662a593f51fcb39394305
이아현 <suk03029@gmail.com>
2021-11-25 16:05:44 +0900

    태그를 입력하는 기능 띄워쓰기 할 때마다 태그로 등록/입력값 초기화

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/EditReviewActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/EditReviewActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.retrofitlibrarytest_20211122
 
 import android.os.Bundle
 import android.util.Log
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarytest_20211122.databinding.ActivityEditReviewBinding
 import com.neppplus.retrofitlibrarytest_20211122.datas.BasicResponse
@@ -12,11 +13,13 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 class EditReviewActivity : BaseActivity() {
 
     lateinit var binding: ActivityEditReviewBinding
     lateinit var mProductData : ProductData
+    val mInputTagList = ArrayList<tag>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +31,40 @@ class EditReviewActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+//        한글자를 입력할 때 마다, 스페이스를 넣었는지 검사
+
+        binding.edtTag.addTextChangedListener {
+
+            val nowText = it.toString()
+
+            if (nowText == ""){
+                return@addTextChangedListener
+            }
+            Log.d("입력값",it.toString())
+
+//            지금 입력된 글자에 스페이스가 있는가?
+
+            if (nowText.last() == ' ' ){
+
+                Log.d("입력값","스페이스가 들어옴")
+
+//                입력된 값을 태그로 등록(공백 제거 후 사용)
+               val tag = nowText.replace(" ", "")
+//                태그 목록으로 추가해보자
+                mInputTagList.add(tag)
+                binding.edtTag.setText("")
+
+
+            }
+
+        }
+
         binding.btnWrite.setOnClickListener{
+
+            for (tag in mInputTagList){
+                Log.d("입력태그",tag)
+            }
+            return@setOnClickListener
 
             val inputTitle = binding.edtReviewTitle.text.toString()
             val inputContent = binding.edtReviewContent.text.toString()
