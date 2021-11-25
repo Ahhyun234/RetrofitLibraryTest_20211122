d41eccff7eb10aaa4931750cd8debcc33792118c
이아현 <suk03029@gmail.com>
2021-11-25 15:44:32 +0900

    사용자 정보에서도 date로 파싱   -> string -> date변환 -> 시차 보정 다 되어있음

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/datas/UserData.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/datas/UserData.kt
@@ -2,6 +2,7 @@ package com.neppplus.retrofitlibrarytest_20211122.datas
 
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
+import java.util.*
 
 class UserData(
 
@@ -10,11 +11,16 @@ class UserData(
     var provider : String,
     var uid : String?,
 
+    @SerializedName("created_at")
+    var createdAt : Date,
+
 //    서버의 이름표와 변수의 이름이 다를때
     @SerializedName("nick_name")
     var nickname: String,
     @SerializedName("profile_img")
     var profileImageURL: String
+
+
 
 ) :Serializable {
 }
