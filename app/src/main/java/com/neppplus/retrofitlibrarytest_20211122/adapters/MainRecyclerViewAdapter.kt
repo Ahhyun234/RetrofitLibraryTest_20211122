7f37e2b89dd6f9927542588bbd211f8a89c2dc2e
이아현 <suk03029@gmail.com>
2021-11-30 13:03:40 +0900

    각각 지정함

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/adapters/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/adapters/MainRecyclerViewAdapter.kt
@@ -1,9 +1,11 @@
 package com.neppplus.retrofitlibrarytest_20211122.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.retrofitlibrarytest_20211122.R
 
 class MainRecyclerViewAdapter(val mContext : Context , val mList : List ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 //    두가지 뷰홀더가 필요함. -> 0번칸 : 상단부(header) xml / 나머지 칸(item) : 리뷰모양 XML
@@ -28,6 +30,19 @@ class MainRecyclerViewAdapter(val mContext : Context , val mList : List ) : Recy
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        return when(viewType){
+            HEADER_VIEW_TYPE -> {
+                val row = LayoutInflater.from(mContext).inflate(R.layout.main_recycler_item_top_view,parent,false)
+                headerViewHolder(row)
+
+            }
+            else-> {
+                val row= LayoutInflater.from(mContext).inflate(R.layout.main_recyclerview_review_item,parent,false)
+                itemViewHolder(row)
+//                리뷰 아이템
+            }
+        }
 
     }
 
