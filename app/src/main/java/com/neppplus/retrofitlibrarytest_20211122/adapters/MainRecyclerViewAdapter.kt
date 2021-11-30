36abff2e1db12d422fda4559b78c2551cc78e83b
이아현 <suk03029@gmail.com>
2021-11-30 15:49:52 +0900

    2초마다 ui 스레드에서 반복하도록 코드 구조 설정

--- app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/adapters/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarytest_20211122/adapters/MainRecyclerViewAdapter.kt
@@ -1,6 +1,8 @@
 package com.neppplus.retrofitlibrarytest_20211122.adapters
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +14,8 @@ import com.neppplus.retrofitlibrarytest_20211122.MainActivity
 import com.neppplus.retrofitlibrarytest_20211122.R
 import com.neppplus.retrofitlibrarytest_20211122.datas.BannerData
 import com.neppplus.retrofitlibrarytest_20211122.datas.ReviewData
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainRecyclerViewAdapter(val mContext : Context , val mList : List<ReviewData> ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -20,6 +24,8 @@ class MainRecyclerViewAdapter(val mContext : Context , val mList : List<ReviewDa
 
 //    상단 뷰페이저 어댑터 변수 -> 객체는 bind에서 생성
     lateinit var bannerViewPagerAdapter: BannerViewPagerAdapter
+
+
 
 //    두가지 뷰홀더가 필요함. -> 0번칸 : 상단부(header) xml / 나머지 칸(item) : 리뷰모양 XML
     inner class headerViewHolder(row:View): RecyclerView.ViewHolder(row){
@@ -36,6 +42,28 @@ class MainRecyclerViewAdapter(val mContext : Context , val mList : List<ReviewDa
         bannerViewPagerAdapter = BannerViewPagerAdapter((mContext as MainActivity).supportFragmentManager, mBannerList)
 
         bannerViewPager.adapter=bannerViewPagerAdapter
+
+//        완성된 배너 어댑터에 2초마다 자동으로 다음 그림으로 넘어가게
+//        >할일 코드 생성
+            val nextPage = {
+
+            } //Runnable (할일이 담긴 코드)
+
+//        타이머 안에서 할 일을 UI스레드로 전달해주는 도구 만들기 (handler)
+        val myHandler = Handler(Looper.getMainLooper())
+
+//        Timer클래스 활용 => 할일(코드)를 2초 마다 반복
+
+        val timer = Timer()
+        timer.schedule(object :TimerTask(){
+            override fun run() {
+//                반복적으로 수행할 코드 => UI를 조작하면 앱이 죽음
+//                ui스레드에 NextPage에 작힌 할일 실행하도록 넘겨주자
+                myHandler.post(nextPage)
+
+            }
+
+        },2000 ,2000)
 
         imgCategory1.setOnClickListener {
 
